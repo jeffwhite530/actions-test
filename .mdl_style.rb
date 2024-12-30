2a43f01de5992327b9a67f87177542cc95e9bc84
Jeff White <44498114+jeffwhite530@users.noreply.github.com>
2024-12-30 14:56:31 -0500

    Markdown

---
+++ .mdl_style.rb
@@ -0,0 +1,12 @@
+all
+
+exclude_rule 'MD001'
+
+# Multiple consecutive blank lines
+exclude_rule 'MD012'
+
+# Bare URL use
+exclude_rule 'MD034'
+
+# Trailing punctuation in headers
+rule 'MD026', punctuation: '.,;:!'
