fa55707c39711c5f0461377f56e792a2349eaa08
Jeff White <44498114+jeffwhite530@users.noreply.github.com>
2024-12-30 14:44:50 -0500

    Markdown

---
+++ .mdlstyle.rb
@@ -0,0 +1,22 @@
+all
+
+# Multiple consecutive blank lines
+exclude_rule 'MD012'
+
+# Headers should be surrounded by blank lines
+exclude_rule 'MD022'
+
+# Fenced code blocks should be surrounded by blank lines
+exclude_rule 'MD031'
+
+# Lists should be surrounded by blank lines
+exclude_rule 'MD032'
+
+# Bare URL use
+exclude_rule 'MD034'
+
+# First line in file should be a top level header
+exclude_rule 'MD041'
+
+# Trailing punctuation in headers
+rule 'MD026', punctuation: '.,;:!'
